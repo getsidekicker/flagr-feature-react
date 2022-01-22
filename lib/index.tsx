8a61e19d44091478291e448907ed890cf202c24b
Tristan Kenney <tristan@kenney.co>
2022-01-22 12:04:08 +1100

    Added entity id to Context Provider

--- lib/index.tsx
+++ lib/index.tsx
@@ -19,6 +19,7 @@ interface FlagrContextProviderProps {
   children: React.ReactNode;
   flagrUrl: string;
   tags: [string, ...string[]];
+  id?: string;
   context?: JsonObject;
 }
 
@@ -26,6 +27,7 @@ export const FlagrContextProvider = ({
   children,
   flagrUrl,
   tags,
+  id,
   context = {},
 }: FlagrContextProviderProps) => {
   const [value, setValue] = useState<FlagrContextType>({ loaded: false });
@@ -37,6 +39,7 @@ export const FlagrContextProvider = ({
       });
 
       const { cachedEvaluate, cachedMatch } = await Evaluator.batchEvaluation({
+        id,
         context,
         input: {
           tags,
